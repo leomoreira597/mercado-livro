9ec5008e15294927d35825d1c3991f888277275d
Leonardo Moreira <lmoreira597@gmail.com>
2022-07-27 18:26:37 -0300

    Criação da data class

--- src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
+++ src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
@@ -1,14 +1,18 @@
 package com.mercadolivro.controller
 
+import com.mercadolivro.model.CustomerModel
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@RequestMapping("customer")
 class CustomerController {
 
     @GetMapping
-    fun helloWorld(): String{
-        return  "Hello World"
+    fun getCustomer(): CustomerModel{
+        return CustomerModel("1", "Leonardo", "lmoreira597@gmail.com")
     }
+
 
 }
