4fe84e1b222616622b1568d9e0088f3a9f7990d3
Leonardo Moreira <lmoreira597@gmail.com>
2022-08-01 13:51:33 -0300

    Adicionando todos os metodos de book

--- src/main/kotlin/com/mercadolivro/controller/BookController.kt
+++ src/main/kotlin/com/mercadolivro/controller/BookController.kt
@@ -1,11 +1,18 @@
 package com.mercadolivro.controller
 
 import com.mercadolivro.controller.request.PostBookRequest
+import com.mercadolivro.controller.request.PutBookRequest
+import com.mercadolivro.enums.BookStatus
 import com.mercadolivro.extension.toBookModel
+import com.mercadolivro.model.BookModel
 import com.mercadolivro.service.BookService
 import com.mercadolivro.service.CustomerService
 import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseStatus
@@ -23,5 +30,34 @@ class BookController(
     fun create(@RequestBody request: PostBookRequest){
         val customer = customerService.getById(request.customerId)
         bookService.create(request.toBookModel(customer))
+    }
+
+    @GetMapping
+    fun getAll(): List<BookModel> {
+        return bookService.findAll()
+    }
+
+    @GetMapping("/active")
+    fun findActives(): List<BookModel>{
+        return bookService.findActives()
+    }
+
+    @GetMapping("/{id}")
+    fun findById(@PathVariable id: Int): BookModel{
+        return bookService.findById(id)
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun delete(@PathVariable id: Int){
+        bookService.delete(id)
+
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun update(@PathVariable id: Int, @RequestBody book: PutBookRequest){
+        val bookSaved = bookService.findById(id)
+        bookService.update(book.toBookModel(bookSaved))
     }
 }
