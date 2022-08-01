df79e4c245566bd68080f5cb30e0b89b31bb9ad2
Leonardo Moreira <lmoreira597@gmail.com>
2022-08-01 00:27:48 -0300

    implementando o repository

--- src/main/kotlin/com/mercadolivro/model/CustomerModel.kt
+++ src/main/kotlin/com/mercadolivro/model/CustomerModel.kt
@@ -1,12 +1,14 @@
 package com.mercadolivro.model
 
+import org.hibernate.Hibernate
+import org.springframework.lang.NonNull
 import javax.persistence.Column
 import javax.persistence.Entity
 import javax.persistence.GeneratedValue
 import javax.persistence.GenerationType
 import javax.persistence.Id
 
-@Entity(name = "Customer")
+@Entity(name = "customer")
 data class CustomerModel(
     @Id
     @GeneratedValue(strategy =  GenerationType.IDENTITY)
--- src/main/kotlin/com/mercadolivro/service/CustomerService.kt
+++ src/main/kotlin/com/mercadolivro/service/CustomerService.kt
@@ -3,48 +3,46 @@ package com.mercadolivro.service
 import com.mercadolivro.controller.request.PostCustomerRequest
 import com.mercadolivro.controller.request.PutCustomerRequest
 import com.mercadolivro.model.CustomerModel
+import com.mercadolivro.repository.CustomerRepository
 import org.springframework.stereotype.Service
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestParam
 
 @Service
-class CustomerService {
-
-    val customers = mutableListOf<CustomerModel>()
+class CustomerService(
+    val customerRepository: CustomerRepository
+) {
 
     fun getAll(name: String?): List<CustomerModel> {
         name?.let {
-            return customers.filter { it.name.contains(name, true) }
+            return customerRepository.findBynameContaining(it)
         }
-        return customers
+        return customerRepository.findAll().toList()
     }
 
-    fun create( customer: CustomerModel) {
-        val id = if (customers.isEmpty()) {
-            1
-        } else {
-            customers.last().id!!.toInt() + 1
-        }
-
-        customer.id = id
-
-        customers.add(customer)
+    fun create(customer: CustomerModel) {
+        customerRepository.save(customer)
     }
 
     fun getCustomer(id: Int): CustomerModel {
-        return customers.filter { it.id == id }.first()
+        return customerRepository.findById(id).orElseThrow()
     }
 
     fun update(customer: CustomerModel) {
-        customers.filter { it.id == customer.id }.first().let {
-            it.name = customer.name
-            it.email = customer.email
+        if(!customerRepository.existsById(customer.id!!)){
+            throw Exception()
         }
+
+        customerRepository.save(customer)
     }
 
     fun delete(id: Int) {
-        customers.removeIf { it.id == id }
+        if(!customerRepository.existsById(id)){
+            throw Exception()
+        }
+
+        customerRepository.deleteById(id)
     }
 
 }
