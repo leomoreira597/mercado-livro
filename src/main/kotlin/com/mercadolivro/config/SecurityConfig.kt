fe122b2fc4aea9982b4c938f5b4930ad92a23cb4
Leonardo Moreira <lmoreira597@gmail.com>
2022-08-17 19:44:17 -0300

    inserindo permisão de admin

--- src/main/kotlin/com/mercadolivro/config/SecurityConfig.kt
+++ src/main/kotlin/com/mercadolivro/config/SecurityConfig.kt
@@ -1,5 +1,6 @@
 package com.mercadolivro.config
 
+import com.mercadolivro.enums.Role
 import com.mercadolivro.repository.CustomerRepository
 import com.mercadolivro.security.AuthenticationFilter
 import com.mercadolivro.security.AuthorizationFilter
@@ -28,6 +29,10 @@ class SecurityConfig(
         "/customer"
     )
 
+    private val ADMIN_MATCHERS = arrayOf(
+        "/admin/**"
+    )
+
     override fun configure(auth: AuthenticationManagerBuilder) {
         auth.userDetailsService(userDetails).passwordEncoder(bCryptPasswordEncoder())
     }
@@ -37,6 +42,7 @@ class SecurityConfig(
 
         http.authorizeRequests()
             .antMatchers(HttpMethod.POST, *PUBLIC_POST_MATCHERS).permitAll()
+            .antMatchers(*ADMIN_MATCHERS).hasAuthority(Role.ADMIN.description)
             .anyRequest().authenticated()
             http.addFilter(AuthenticationFilter(authenticationManager(), customerRepository, jwtUtil))
             http.addFilter(AuthorizationFilter(authenticationManager(), userDetails, jwtUtil))
